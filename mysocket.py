ac02eeb7da1e626d5e541eb6b134a46b13cfae64
ctm002 <carlostapiam@gmail.com>
2017-03-02 11:45:38 -0300

    se agregar function que permite conectarse a cualquier interface de red.

--- mysocket.py
+++ mysocket.py
@@ -1,31 +1,54 @@
 import socket
+import re
+import fcntl
+import struct
+
 from filewriter import FileWriter
 from crawler import Crawler
-import re
+from parametrosconsultamarca import ParametrosConsultaMarca
+from parametros import Parametros
+from convert import Convert
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+def Post():
+	host = "ion.inapi.cl"
+	port = 8080
+	headers = """\
+	POST http://ion.inapi.cl:8080/Marca/BuscarMarca.aspx/FindMarcas HTTP/1.1\nHost:{host}\nConnection:{connection}\nContent-Length:{content_length}\nUserAgent:{user_agent}\nContent-Type:{content_type}\nReferer:{referer}\nCookie:{cookie};\n\n"""
+
+	pHash, pIDW, pCookie = Get() 
+	parametros = Convert().tojson(ParametrosConsultaMarca(pIDW, pHash,1181416))
+	body_bytes = parametros.encode('utf-8')
+	header_bytes = headers.format(
+	    content_type="application/json; charset=utf-8",
+	    content_length=len(body_bytes),
+	    user_agent="Mozilla/5.0",
+	    connection="keep-alive",
+	    cookie=pCookie,
+	    host=str(host) + ":" + str(port),
+	    referer="http://ion.inapi.cl:8080/Marca/BuscarMarca.aspx"
+	).encode('utf-8')
+
+	payload = header_bytes + body_bytes
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	ip = (get_ip_address('eth0'), 0)
+	print ip
+	s.bind(ip)
+	s.connect((host, port))
+	s.send(payload)
+	result = s.recv(1024)
 
 def Get():
 	host = "ion.inapi.cl"
 	port = 8080
 
-	# headers = """\
-	# GET /Marca/BuscarMarca.aspx HTTP/1.1\r
-	# Content-Type: {content_type}\r
-	# Content-Length: {content_length}\r
-	# Host: {host}\r
-	# Connection: close\r
-	# \r\n"""
-
-	# body = 'userName=Ganesh&password=pass'                                 
-	# body_bytes = body.encode('ascii')
-	# header_bytes = headers.format(
-	#     content_type="application/json; charset=UTF-8",
-	#     content_length=len(body_bytes),
-	#     host=str(host) + ":" + str(port)
-	# ).encode('iso-8859-1')
-
-	# payload = header_bytes + body_bytes
-	# socket.send (payload)
 
 	total_data=[]
 	headers = b"GET /Marca/BuscarMarca.aspx HTTP/1.1\nHost:{host}\nConnection: close\n\n"
@@ -45,16 +68,13 @@ def Get():
 
 	html = ' '.join(total_data)
 	pHash, pIDW = Crawler().extraer(html)
-
-	print pHash
-	print pIDW
 	nameCookie = "ASP.NET_SessionId="
-	print nameCookie + Crawler().getValueCookie(nameCookie, html)
+	return pHash, pIDW, nameCookie + Crawler().getValueCookie(nameCookie, html)
 
-	fileWrite = FileWriter()
-	fileWrite.open("default.aspx")
-	fileWrite.save(html)
-	fileWrite.close()
+	# fileWrite = FileWriter()
+	# fileWrite.open("default.aspx")
+	# fileWrite.save(html)
+	# fileWrite.close()
 
+Post()
 
-Get()
