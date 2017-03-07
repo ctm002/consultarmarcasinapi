8724adadc69c7fbfd6c87a6ebb41b609a63599af
ctm002 <carlostapiam@gmail.com>
2017-03-07 15:47:49 -0300

    organizando el codigo

--- mysocket.py
+++ mysocket.py
@@ -26,6 +26,7 @@ def Post():
 	pHash, pIDW, pCookie = Get() 
 	parametros = Convert().tojson(ParametrosConsultaMarca(pIDW, pHash,1181416))
 	body_bytes = parametros.encode('utf-8')
+
 	header_bytes = headers.format(
 	    content_type="application/json; charset=utf-8",
 	    content_length=len(body_bytes),
@@ -48,8 +49,6 @@ def Post():
 def Get():
 	host = "ion.inapi.cl"
 	port = 8080
-
-
 	total_data=[]
 	headers = b"GET /Marca/BuscarMarca.aspx HTTP/1.1\nHost:{host}\nConnection: close\n\n"
 	header_bytes = headers.format(
@@ -76,5 +75,57 @@ def Get():
 	# fileWrite.save(html)
 	# fileWrite.close()
 
-Post()
 
+
+
+class MySocket(object):
+
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		self.headers = ""
+		self.body = ""
+		self.url = ""
+		self.cookie = ""
+
+	def setParametros(self, pTipo, pIDW, pHash, pNumero):
+		parametros = ""		
+		if (pTipo == "Registro"):
+			parametros = Convert().tojson(ParametrosConsultaMarca(pIDW, pHash,pNumero))
+		else:
+			parametros = Convert().tojson(ParametrosConsultaSolicitud(pHash, pIDW, pNumero))
+
+		self.body = Convert().tojson(parametros).encode("utf-8")
+		self.content_length = len(self.body)
+
+	def setHeader(self, params):
+		self.headers = """\
+		POST {url} HTTP/1.1\nHost:{host}\nConnection:{connection}\nContent-Length:{content_length}\nUserAgent:{user_agent}\nContent-Type:{content_type}\nReferer:{referer}\nCookie:{cookie};\n\n"""
+		self.headers = headers.format(
+			url = self.url,
+		    content_type="application/json; charset=utf-8",
+		    content_length=self.content_length,
+		    user_agent="Mozilla/5.0",
+		    connection="keep-alive",
+		    cookie=self.cookie,
+		    host=str(self.host) + ":" + str(self.port),
+		    referer="http://ion.inapi.cl:8080/Marca/BuscarMarca.aspx"
+		).encode('utf-8')
+		
+
+	def setCookie(self, cookie):
+		self.cookie = cookie
+	
+	def setURL(self, url):
+		self.url = url
+
+	def downloadData():
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		ip = (get_ip_address('eth0'), 0)
+		print ip
+		s.bind(ip)
+		s.connect((self.host, self.port))
+		payload = self.headers + self.body
+		s.send(payload)
+		result = s.recv(1024)
+		
